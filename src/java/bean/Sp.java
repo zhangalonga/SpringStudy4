ef57d4edc1164daa7fae71d962cba495764ea070
zhangalong <zhangalong@yuewen.com>
2019-02-18 19:06:59 +0800

    起独特的名字

--- src/java/bean/Sp.java
+++ src/java/bean/Sp.java
@@ -2,13 +2,17 @@ package bean;
 
 import org.springframework.stereotype.Component;
 
+
 /**
  * created on 2019/2/18 18:54
  *
  * @author zhangalong
+ * 如果想给他独特的名字，可以使用@Component("lony")
+ * 也可以使用@Named
+ *
  */
 @Component
-public class Sp implements Cd {
+public class Sp implements bean.Cd {
     private String title = "zhangalong";
     private String room = "ri";
 
