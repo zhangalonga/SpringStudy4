f577940f742803d6120ddd68bc8a8e0050f65ce1
zhangalong <zhangalong@yuewen.com>
2019-02-22 15:37:51 +0800

    bean的作用域

--- src/too/Cookies.java
+++ src/too/Cookies.java
@@ -1,9 +1,15 @@
 package too;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 /**
  * created on 2019/2/22 15:19
  *
  * @author zhangalong
  */
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Cookies implements Dessert {
 }
